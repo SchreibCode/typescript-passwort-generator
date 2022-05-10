c726141e42fadd2bd2c2d5a34d48bafd08f7681a
marvinscheffold <m@marvinscheffold.com>
2022-05-10 16:57:40 +0200

    before video

--- src/constants.ts
+++
@@ -1,4 +0,0 @@
-export const NUMBERS: string = "012345679";
-export const SYMBOLS: string = "@!$%&*";
-export const CAPITAL_LETTERS: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-export const LETTERS: string = "abcdefghijklmnopqrstuvwxyz";
